2f57d0a05c2d68ca1b08946386a61926afe7570a
kknudson15 <kyle.knudson2015@gmail.com>
2020-06-25 20:49:52 -0500

    Added I Am Poor Project

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Kyle Knudson on 6/25/20.
+//  Copyright © 2020 Kyle Knudson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
